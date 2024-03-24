671bcdfcaf4c7cc210c2efd3b06ba70acca0638a
Nikita Zelikov <zelaniki@gmail.com>
2024-03-24 19:08:24 +0300

    change webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,115 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
 
-module.exports = {
-    mode: 'development',
-    entry: './index.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(scss|css)$/,
-                use: ['style-loader', 'css-loader'],
+module.exports = ({ production } = {}) => ({
+  mode: production ? 'production' : 'development',
+  entry: 'index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    assetModuleFilename: 'assets/[name][ext]',
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: 'babel-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(scss|css)$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true,
             },
-            {
-                test: /\.(png|jpg|gif)$/i,
-                type: 'asset/resource',
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              sourceMap: true,
             },
-            {
-                test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
-                type: 'asset/resource',
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true,
             },
+          },
         ],
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-          template: path.resolve(__dirname, './index.html'),
-          filename: 'index.html',
-        }),
+      },
+      {
+        test: /\.(woff(2)?|eot|ttf|otf)$/,
+        type: 'asset/resource',
+        generator: {
+          filename: 'assets/fonts/[name].[contenthash][ext]',
+        },
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'assets/icons/[name].[contenthash][ext]',
+        },
+      },
+      {
+        test: /\.html$/i,
+        loader: 'html-loader',
+      },
     ],
-    devServer: {
-        static: './',
-        compress: true,
-        port: 9001,
-    },
-};
+  },
+  optimization: {
+    minimizer: [
+      new CssMinimizerPlugin({
+        minimizerOptions: {
+          preset: [
+            'default',
+            {
+              discardComments: { removeAll: true },
+            },
+          ],
+        },
+      }),
+      new ImageMinimizerPlugin({
+        minimizer: {
+          implementation: ImageMinimizerPlugin.imageminMinify,
+          options: {
+            plugins: [
+              ['gifsicle', { interlaced: true }],
+              ['jpegtran', { progressive: true }],
+              ['optipng', { optimizationLevel: 5 }],
+              ['svgo', { name: 'preset-default' }],
+            ],
+          },
+        },
+      }),
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'index.html'),
+      filename: 'index.html',
+    }),
+    new MiniCssExtractPlugin(),
+    new Dotenv({
+      path: `./.env.${production ? 'production' : 'development'}`,
+    })
+  ],
+  devServer: {
+    historyApiFallback: true,
+    watchFiles: ['*.html'],
+    open: true,
+    compress: true,
+    hot: true,
+    port: 9001,
+  },
+});
